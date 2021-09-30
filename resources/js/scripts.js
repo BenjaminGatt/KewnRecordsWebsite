da7cbe192a77a87d7e82e163a8da7d74a8d3fddb
Benjamin Gatt <benjamin.gatt.f23393@mcast.edu.mt>
2021-09-30 21:30:38 +0200

    niket release update

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -14,8 +14,10 @@ function startTimer() {
 
 let images = [],
   x = -1;
-images[0] = "/resources/img/banner/BILA_Banner_OUT_NOW.jpg";
-images[1] = "/resources/img/banner/Bila_launch_banner_deheb_incens_u.jpg";
-images[2] = "/resources/img/banner/kewn_mixtape - Copy2.png";
-images[3] = "/resources/img/banner/400ppm_banner.jpg";
-images[4] = "/resources/img/banner/Bila_Bestjarju_banner.jpg";
+images[0] = "/resources/img/banner/NIKET-Banner Announcement-min.jpg";
+images[1] = "/resources/img/banner/400ppm-Banner-outnow-min.jpg";
+images[2] = "/resources/img/banner/BILA_Banner_OUT_NOW.jpg";
+images[3] = "/resources/img/banner/Bila_launch_banner_deheb_incens_u.jpg";
+images[4] = "/resources/img/banner/kewn_mixtape - Copy2.png";
+images[5] = "/resources/img/banner/400ppm_banner.jpg";
+images[6] = "/resources/img/banner/Bila_Bestjarju_banner.jpg";
